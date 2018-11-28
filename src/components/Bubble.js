fb5a6353786a984f207d862c414e08a040d35f60
hailong <hailengc@126.com>
2018-11-28 22:07:24 +0800

    refine Buuble class

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -1,27 +1,77 @@
-import Paper, { Path, Point } from "paper";
+import { Path, Color } from "paper";
 
-export default class Bubble extends Path {
-  constructor(position, radius, options) {
-    let initPoints = Bubble.getPoints(position, radius);
-    super({
-      segments: initPoints,
-      strokeColor: "black",
-      closed: true,
-      ...options
-    });
-    this.smooth();
-  }
+const Circle = Path.Circle;
+class Bubble extends Circle {
+  constructor(position, radius) {
+    super(position, radius);
 
-  static getPoints(position, radius) {
-    return [
-      new Point(position.x - radius, position.y),
-      new Point(position.x, position.y + radius),
-      new Point(position.x + radius, position.y),
-      new Point(position.x, position.y - radius)
-    ];
+    this.applyMatrix = false;
+    this.data = {
+      scaleDirection: {
+        x: 1,
+        y: -1
+      },
+      scaleDelta: {
+        x: 0.001,
+        y: 0.002
+      },
+      scaleRange: {
+        x: 0.3,
+        y: 0.3
+      }
+    };
+
+    this.fillColor = {
+      gradient: {
+        stops: [
+          [new Color(1, 1, 1, 0), 0.8],
+          [new Color(1.0, 1.0, 1.0, 1.0), 1]
+        ],
+        radial: true
+      },
+      origin: this.bounds.center,
+      destination: this.bounds.rightCenter
+    };
   }
-  /**
-   * do animate stuff
-   */
-  animate() {}
+  getNextXScaling = () => {
+    let prevXScaling = this.scaling.x;
+
+    let { scaleDirection, scaleRange, scaleDelta } = this.data;
+
+    if (
+      prevXScaling >= 1.0 + scaleRange.x ||
+      prevXScaling < 1.0 - scaleRange.x
+    ) {
+      scaleDirection.x *= -1;
+    }
+    return prevXScaling + scaleDirection.x * scaleDelta.x * 1;
+  };
+
+  getNextYScaling = () => {
+    let prevYScaling = this.scaling.y;
+    let { scaleDirection, scaleRange, scaleDelta } = this.data;
+
+    if (
+      prevYScaling >= 1.0 + scaleRange.y ||
+      prevYScaling < 1.0 - scaleRange.y
+    ) {
+      scaleDirection.y *= -1;
+    }
+    return prevYScaling + scaleDirection.y * scaleDelta.y * 1;
+  };
+
+  setNextScaling = () => {
+    this.scaling = { x: this.getNextXScaling(), y: this.getNextYScaling() };
+  };
+
+  setNextRotate = () => {
+    this.rotate(3 * Math.random());
+  };
+
+  animate = () => {
+    this.setNextScaling();
+    this.setNextRotate();
+  };
 }
+
+export default Bubble;
--- src/components/BubbleCanvas.jsx
+++ src/components/BubbleCanvas.jsx
@@ -3,52 +3,14 @@ import Paper, { Path, Point, Color } from "paper";
 import "./BubbleCanvas.less";
 import Bubble from "./Bubble";
 
-const xScaleRange = {
-  min: 0.97,
-  max: 1.03
-};
-
-const yScaleRange = {
-  min: 0.97,
-  max: 1.03
-};
-const xScaleDelta = 0.001;
-const yScaleDelta = 0.002;
-
 export class BubbleCanvas extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-  getNextScaling(bubble) {
-    let currentXScaling = bubble.scaling.x;
-    let currentYScaling = bubble.scaling.y;
-
-    if (currentXScaling >= xScaleRange.max) {
-      bubble.data.xScaleFactor = -1;
-    } else if (currentXScaling < xScaleRange.min) {
-      bubble.data.xScaleFactor = 1;
-    }
-    let xs = Math.abs(bubble.data.xScaleFactor - 1.0) * xScaleDelta * 2;
-
-    if (currentYScaling >= yScaleRange.max) {
-      bubble.data.yScaleFactor = -1;
-    } else if (currentYScaling < yScaleRange.min) {
-      bubble.data.yScaleFactor = 1;
-    }
-    let ys = Math.abs(bubble.data.yScaleFactor - 1.0) * yScaleDelta * 2;
-
-    return {
-      x: currentXScaling + bubble.data.xScaleFactor * xs,
-      y: currentYScaling + bubble.data.yScaleFactor * ys
-    };
-  }
 
   animate = event => {
-    this.bubble.scaling = this.getNextScaling(this.bubble);
-    this.bubble.rotate(3 * Math.random());
-
-    // this.bbox = this.bbox.remove() && this.getBBoxFromBubble(this.bubble);
+    this.bubble.animate();
   };
 
   getBBoxFromBubble(bubble) {
@@ -64,8 +26,6 @@ export class BubbleCanvas extends Component {
     this.paperProject = Paper.project;
     this.paperView = Paper.view;
 
-    this.paperView.onFrame = this.animate;
-
     // layers
     this.rasterLayer = new Paper.Layer();
     this.paintLayer = new Paper.Layer();
@@ -74,30 +34,8 @@ export class BubbleCanvas extends Component {
     this.background = new Path.Rectangle(this.paperView.bounds);
     this.background.fillColor = "darkblue";
 
-    // settings for the bubble
-    this.bubble = new Path.Circle(new Point(400, 200), 80);
-    this.bubble.applyMatrix = false;
-    // this.bubble.selected = true;
-    this.bubble.fillColor = {
-      gradient: {
-        stops: [
-          [new Color(1, 1, 1, 0), 0.8],
-          [new Color(1.0, 1.0, 1.0, 1.0), 1]
-        ],
-        radial: true
-      },
-      origin: this.bubble.bounds.center,
-      destination: this.bubble.bounds.rightCenter
-    };
-    // this.bubble.strokeColor = "#eaf1fc";
-    // this.bubble.strokeWidth = 5;
-
-    this.bubble.data = {
-      xScaleFactor: 1,
-      yScaleFactor: -1
-    };
-
-    console.log(this.bbox);
+    this.bubble = new Bubble(new Point(400, 200), 80);
+    this.paperView.onFrame = this.animate;
   };
 
   render() {
